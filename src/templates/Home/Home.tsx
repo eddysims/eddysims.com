100a47d84e52d97c98268295bf8d5053aff90cc9
eddysims <eddy.s@getjobber.com>
2022-02-28 14:13:49 -0700

    new home page

--- src/templates/Home/Home.tsx
+++ src/templates/Home/Home.tsx
@@ -1,3 +1,24 @@
+import styles from "./Home.module.css";
+
 export function Home() {
-  return <div>Home</div>
+  return (
+    <div className={styles.main}>
+      <div className={styles.spread}>
+        {`{...`}
+        <Heart />
+        {`}`}
+      </div>
+      <h1 className={styles.h1}>
+        Eddy Sims<span className={styles.heart}>.</span>
+      </h1>
+      <p className={styles.p}>
+        Front end developer with over 15 years of experience. Looking to
+        experiment here, and show off some of my personal work.
+      </p>
+    </div>
+  );
+}
+
+function Heart() {
+  return <span className={styles.heart}>♥️</span>;
 }
