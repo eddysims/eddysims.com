3c087903b45871eb97d712892eb14764ac717042
eddysims <eddysimsweb@gmail.com>
2023-05-26 23:00:14 -0600

    feat: add content to home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,34 +5,14 @@ import { LayoutCentered } from "@/components/shared/LayoutCentered";
 export default function Home() {
   return (
     <LayoutCentered>
-      <Container>Content</Container>
-      <main>
-        {/* <nav>
-          <ul className="flex justify-end gap-lg py-lg">
-            <li>
-              <a href="https://www.linkedin.com/in/edeesims/" target="_blank">
-                <Icon name="linkedin" size="large" />
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/eddysims" target="_blank">
-                <Icon name="github" size="large" />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/eddysims/eddysims.com"
-                target="_blank"
-              >
-                <Icon name="code" size="large" />
-              </a>
-            </li>
-          </ul>
-        </nav>
-        Eddy Sims Edmonton based software developer with over 15 years of
-        experience. Currently for hire. Looking to show off some work,
-        experiment, and share my learnings. */}
-      </main>
+      <Container>
+        <h1>Eddy Sims</h1>
+        <p>
+          Edmonton based software developer with over 15 years of experience.
+          Currently for hire. Looking to show off some work, experiment, and
+          share my learnings.
+        </p>
+      </Container>
     </LayoutCentered>
   );
 }
