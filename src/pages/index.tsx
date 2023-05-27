f627473774b8771d7b10ca32b4a68071744f6cc4
eddysims <eddysimsweb@gmail.com>
2023-05-26 23:07:59 -0600

    feat: adds a Heading component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,19 @@
-// import { Icon } from "@/components/shared/Icon";
 import { Container } from "@/components/shared/Container";
+import { Heading } from "@/components/shared/Heading";
 import { LayoutCentered } from "@/components/shared/LayoutCentered";
 
 export default function Home() {
   return (
     <LayoutCentered>
       <Container>
-        <h1>Eddy Sims</h1>
-        <p>
-          Edmonton based software developer with over 15 years of experience.
-          Currently for hire. Looking to show off some work, experiment, and
-          share my learnings.
-        </p>
+        <div className="grid gap-5 lg:gap-8 text-center lg:text-left">
+          <Heading as="h1">Eddy Sims</Heading>
+          <p>
+            Edmonton based software developer with over 15 years of experience.
+            Currently for hire. Looking to show off some work, experiment, and
+            share my learnings.
+          </p>
+        </div>
       </Container>
     </LayoutCentered>
   );
