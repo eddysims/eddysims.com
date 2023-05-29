ef5a359cac88e8d82620a07b45e97e949575b884
eddysims <eddysimsweb@gmail.com>
2023-05-28 21:57:51 -0600

    feat: add favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 
 import { AppProps } from "next/app";
 import { Raleway } from "next/font/google";
+import NextHead from "next/head";
 
 const raleway = Raleway({
   weight: ["100", "200", "300", "400", "900"],
@@ -18,6 +19,34 @@ function MyApp({ Component, pageProps }: AppProps) {
           }
         `}
       </style>
+      <NextHead>
+        <title>Eddy Sims</title>
+        <meta
+          name="description"
+          content="Eddy Sims, Software and Web3 Engineer in Edmonton, Alberta."
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#a3160a" />
+        <meta name="msapplication-TileColor" content="#f2ede7" />
+        <meta name="theme-color" content="#f2ede7" />
+      </NextHead>
       <Component {...pageProps} />
     </>
   );
