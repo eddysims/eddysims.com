99184a8e31a10f3dbe45b7730bcdc26461a7c74a
Eddy Sims <46460840+eddysims@users.noreply.github.com>
2023-05-28 22:36:57 -0600

    feat: add favicons for color modes (#3)
    * feat: add color mode favicons
    
    * feat: add color mode favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,18 @@ import { AppProps } from "next/app";
 import { Raleway } from "next/font/google";
 import NextHead from "next/head";
 
+import { useSystemTheme } from "@/hooks/useSystemTheme";
+
 const raleway = Raleway({
   weight: ["100", "200", "300", "400", "900"],
   subsets: ["latin"],
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const colorMode = useSystemTheme("light");
+
+  console.log({ colorMode });
+
   return (
     <>
       <style jsx global>
@@ -28,22 +34,26 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href={`/favicon/${colorMode}/apple-touch-icon.png`}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href={`/favicon/${colorMode}/favicon-32x32.png`}
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href={`/favicon/${colorMode}/favicon-16x16.png`}
         />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#a3160a" />
+        <link rel="manifest" href={`/favicon/${colorMode}/site.webmanifest`} />
+        <link
+          rel="mask-icon"
+          href={`/favicon/${colorMode}/safari-pinned-tab.svg`}
+          color="#a3160a"
+        />
         <meta name="msapplication-TileColor" content="#f2ede7" />
         <meta name="theme-color" content="#f2ede7" />
       </NextHead>
