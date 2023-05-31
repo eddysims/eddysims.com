eb703e08a9a2f916a9436d8a837cc891521cb0d4
Eddy Sims <46460840+eddysims@users.noreply.github.com>
2023-05-31 09:25:24 -0600

    feat: add the Tooltip component (#6)
    * feat: add the tooltip component
    
    * feat: make the tooltip more accessible
    
    * feat: update the stories

---
+++ src/components/shared/Tooltip/Tooltip.tsx
@@ -0,0 +1,94 @@
+import { Placement } from "@popperjs/core";
+import clsx from "clsx";
+import {
+  useRef,
+  useState,
+  PropsWithChildren,
+  useId,
+  KeyboardEvent,
+} from "react";
+import { usePopper } from "react-popper";
+
+type TooltipProps = {
+  message: string;
+  placement?: Placement;
+};
+
+export function Tooltip({
+  children,
+  message,
+  placement = "top",
+}: PropsWithChildren<TooltipProps>) {
+  const id = useId();
+  const [open, setOpen] = useState<boolean>();
+  const [reference, setReference] = useState<HTMLDivElement | null>();
+  const [popperEl, setPopperEl] = useState<HTMLDivElement | null>();
+
+  const { styles, attributes, state } = usePopper(reference, popperEl, {
+    placement,
+    modifiers: [{ name: "offset", options: { offset: [0, 12] } }],
+  });
+
+  const enterTimeout = useRef<NodeJS.Timeout>();
+  const enterDelay = 100;
+  const leaveTimeout = useRef<NodeJS.Timeout>();
+  const leaveDelay = 250;
+
+  const handleMouseEnter = () => {
+    leaveTimeout.current && clearTimeout(leaveTimeout.current);
+    enterTimeout.current = setTimeout(() => setOpen(true), enterDelay);
+  };
+
+  const handleMouseLeave = () => {
+    enterTimeout.current && clearTimeout(enterTimeout.current);
+    leaveTimeout.current = setTimeout(() => setOpen(false), leaveDelay);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.code === "Escape") {
+      handleMouseLeave();
+    }
+  };
+
+  return (
+    <>
+      <div
+        ref={setReference}
+        onMouseEnter={handleMouseEnter}
+        onFocus={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onBlur={handleMouseLeave}
+        onKeyDown={handleKeyDown}
+        className="relative cursor-help"
+        id={id}
+        tabIndex={0}
+      >
+        {children}
+      </div>
+
+      <div
+        {...attributes.popper}
+        ref={setPopperEl}
+        style={styles.popper}
+        className={toolTipClass(open, state?.placement)}
+        role="tooltip"
+        aria-describedby={id}
+        tabIndex={-1}
+      >
+        {message}
+      </div>
+    </>
+  );
+}
+
+const toolTipClass = (open?: boolean, placement?: Placement) =>
+  clsx(
+    " text-center bg-white max-w-xs text-xs rounded border-border border transition-opacity p-3 shadow-lg leading-relaxed",
+    open ? "visible opacity-100" : "invisible opacity-0",
+    {
+      "arrow-left": placement === "right",
+      "arrow-bottom": placement === "top",
+      "arrow-top": placement === "bottom",
+      "arrow-right": placement === "left",
+    }
+  );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
 const { spacing, fontFamily } = require("tailwindcss/defaultTheme");
 
 const baseSpacing = spacing["5"];
+const borderColor = colors.neutral["200"];
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,6 +18,7 @@ module.exports = {
       colors: {
         surface: "#f2ede7",
         primary: "#a3160a",
+        border: borderColor,
       },
       fontFamily: {
         base: ["var(--raleway-font)", ...fontFamily.sans],
@@ -38,6 +43,87 @@ module.exports = {
           },
         },
       });
+    },
+    function ({ addComponents }) {
+      const arrowStyles = { position: "relative" };
+      const arrowPseudoStyles = {
+        content: '""',
+        position: "absolute",
+        width: 0,
+        height: 0,
+      };
+      const arrows = {
+        ".arrow-left": {
+          ...arrowStyles,
+          "&::after, &::before": {
+            ...arrowPseudoStyles,
+            left: 0,
+            top: "50%",
+            transform: "translate(-100%, -50%)",
+            borderTop: "5px solid transparent",
+            borderBottom: "5px solid transparent",
+            borderRight: "8px solid white",
+          },
+          "&::before": {
+            borderTop: "6px solid transparent",
+            borderBottom: "6px solid transparent",
+            borderRight: `9px solid ${borderColor}`,
+          },
+        },
+        ".arrow-right": {
+          ...arrowStyles,
+          "&::after, &::before": {
+            ...arrowPseudoStyles,
+            right: 0,
+            top: "50%",
+            transform: "translate(100%, -50%)",
+            borderTop: "5px solid transparent",
+            borderBottom: "5px solid transparent",
+            borderLeft: "8px solid white",
+          },
+          "&::before": {
+            borderTop: "6px solid transparent",
+            borderBottom: "6px solid transparent",
+            borderLeft: `9px solid ${borderColor}`,
+          },
+        },
+        ".arrow-bottom": {
+          ...arrowStyles,
+          "&::after, &::before": {
+            ...arrowPseudoStyles,
+            left: "50%",
+            bottom: 0,
+            transform: "translate(-50%, 100%)",
+            borderLeft: "5px solid transparent",
+            borderRight: "5px solid transparent",
+            borderTop: "8px solid white",
+          },
+          "&::before": {
+            borderLeft: "6px solid transparent",
+            borderRight: "6px solid transparent",
+            borderTop: `9px solid ${borderColor}`,
+          },
+        },
+        ".arrow-top": {
+          ...arrowStyles,
+          "&::after, &::before": {
+            ...arrowPseudoStyles,
+            left: "50%",
+            top: 0,
+            transform: "translate(-50%, -100%)",
+            borderLeft: "5px solid transparent",
+            borderRight: "5px solid transparent",
+            borderBottom: "8px solid white",
+          },
+          "&::before": {
+            borderLeft: "6px solid transparent",
+            borderRight: "6px solid transparent",
+            borderBottom: `9px solid ${borderColor}`,
+          },
+        },
+      };
+
+      addComponents(arrows);
     },
   ],
 };
