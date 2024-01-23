6c386c1aeb45891cdb4e1a33d208c9fef351fe1f
eddysims <eddysimsweb@gmail.com>
2024-01-22 22:31:05 -0700

    add font subsets

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,6 +1,6 @@
 import { Lilita_One, Comfortaa } from "next/font/google";
 
-const display = Lilita_One({ weight: "400" });
+const display = Lilita_One({ weight: "400", subsets: ['latin'] });
 const body = Comfortaa({subsets: ['latin']});
 
 export { display, body };
