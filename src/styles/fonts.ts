8341653359b428417f68e4a9945f574b54bbb8e2
eddysims <eddysimsweb@gmail.com>
2024-01-22 22:28:04 -0700

    update font file

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,4 +1,6 @@
 import { Lilita_One, Comfortaa } from "next/font/google";
 
-export const display = Lilita_One({ weight: "400" });
-export const body = Comfortaa();
+const display = Lilita_One({ weight: "400" });
+const body = Comfortaa();
+
+export { display, body };
