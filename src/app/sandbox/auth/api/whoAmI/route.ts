3613ad83d11d198a414b4b5183b7096c88e8d8d3
eddysims <eddysimsweb@gmail.com>
2024-02-13 15:27:15 -0700

    move authoptions

--- src/app/sandbox/auth/api/whoAmI/route.ts
+++ src/app/sandbox/auth/api/whoAmI/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "../../../../api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 
 export async function GET() {
   const session = await getServerSession(authOptions);
