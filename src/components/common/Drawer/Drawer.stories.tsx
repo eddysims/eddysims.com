44e5e297783137de6042fbcf1193649a712c44cf
Eddy Sims <46460840+eddysims@users.noreply.github.com>
2024-02-23 11:10:51 -0700

    adds the drawer component (#14)

---
+++ src/components/common/Drawer/Drawer.stories.tsx
@@ -0,0 +1,40 @@
+import { useArgs } from "@storybook/preview-api";
+
+import { Button } from "@/components/Button";
+
+import { Drawer } from ".";
+
+import type { Meta } from "@storybook/react";
+
+/**
+ * ## Usage
+ *
+ * ```
+ * import { Drawer } from "@/components/Drawer";
+ * ```
+ */
+
+const meta: Meta<typeof Drawer> = {
+  title: "components/Drawer",
+  component: Drawer,
+  render: (args) => <Default {...args} />,
+};
+
+export const Default = ({ ...args }) => {
+  const [{ open }, updateArgs] = useArgs();
+
+  return (
+    <>
+      <Button label="Open Drawer" onClick={() => updateArgs({ open: true })} />
+      <Drawer
+        {...args}
+        open={open}
+        onRequestClose={() => {
+          updateArgs({ open: false });
+        }}
+      />
+    </>
+  );
+};
+
+export default meta;
