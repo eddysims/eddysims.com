cc9230d42501c727fb20a9746f40b8261ebba392
Eddy Sims <46460840+eddysims@users.noreply.github.com>
2024-02-25 14:04:22 -0700

    add a favicon (#18)
    * adds the favicon
    
    * add the favicon to storybook
    
    * add all favicon to storybook
    
    * udpate storybook
    
    * fix storybook favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 
 import clsx from "clsx";
+import Head from "next/head";
 import { getServerSession } from "next-auth";
 import { PropsWithChildren } from "react";
 
@@ -25,6 +26,29 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#171820" />
+        <meta name="msapplication-TileColor" content="#171820" />
+        <meta name="theme-color" content="#171820" />
+      </Head>
       <body className={clsx(display.variable, body.variable)}>
         <SessionProvider session={session}>
           <ContactDrawerProvider>
