c36d13ae747062abedbdc12d46a767b563b1e18a
eddysims <eddysimsweb@gmail.com>
2024-02-27 23:11:42 -0700

    add theme

---
+++ .storybook/theme.ts
@@ -0,0 +1,17 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "dark",
+  brandUrl: "https://eddysims.com",  
+  colorPrimary: "#D41100",
+  colorSecondary: "#D41100",
+  appBg: "#121319",
+  appContentBg: "#171820",
+  appBorderColor: "#121319",
+  appBorderRadius: 5,
+  appPreviewBg: "#171820",
+  barTextColor: "#F2EDE7",
+  barSelectedColor: "#D41100",
+  barBg: "#171820",
+  barHoverColor: "#D41100",
+});
