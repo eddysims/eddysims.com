09e1cdcd9da93ce88d11e3cf84049ee3ad43862d
Eddy Sims <46460840+eddysims@users.noreply.github.com>
2024-03-12 13:57:52 -0600

    make resume available (#31)
    * adds the beginning of the Navigation
    
    * add resume link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,49 +10,52 @@ import { SocialLinks } from "@/components/social/SocialLinks";
 
 export default function Home() {
   return (
-    <Container>
-      <div className="lg:grid lg:grid-cols-6 lg:min-h-screen">
+    <Container
+      className={clsx(
+        "flex-1 flex flex-col justify-center",
+        "lg:grid lg:grid-cols-6 lg:min-h-full",
+      )}
+    >
+      <div
+        className={clsx(
+          "grid gap-3 text-center",
+          "lg:col-span-3 lg:self-center lg:text-left",
+          "xl:col-span-2",
+        )}
+      >
         <div
           className={clsx(
-            "grid gap-3 text-center",
-            "lg:col-span-3 lg:self-center lg:text-left",
-            "xl:col-span-2",
+            "flex flex-col items-center gap-5 mb-5",
+            "lg:flex-row lg:gap-12 lg:mb-3",
           )}
         >
-          <div
-            className={clsx(
-              "flex flex-col items-center gap-5 mb-5",
-              "lg:flex-row lg:gap-12 lg:mb-3",
-            )}
-          >
-            <div className="order-2 lg:order-1">
-              <Heading as="h1" style="h3">
-                Eddy Sims
-              </Heading>
-            </div>
-            <div className="text-primary order-1">
-              <SocialLinks />
-            </div>
+          <div className="order-2 lg:order-1">
+            <Heading as="h1" style="h3">
+              Eddy Sims
+            </Heading>
           </div>
-          <Heading style="h1">Software Engineer</Heading>
-          <div className="my-5">
-            <Text>
-              Helping people turn their ideas into sites &amp; apps that work.
-            </Text>
-          </div>
-          <div className={clsx("mx-auto text-center", "lg:mx-0")}>
-            <ContactDrawerButton label="Chat with me" />
+          <div className="text-primary order-1">
+            <SocialLinks />
           </div>
         </div>
-        <div
-          className={clsx(
-            "relative hidden",
-            "lg:block lg:col-span-3",
-            "xl:col-start-4",
-          )}
-        >
-          <Image src="./images/eddy.svg" alt="Eddy Sims" fill />
+        <Heading style="h1">Software Engineer</Heading>
+        <div className="my-5">
+          <Text>
+            Helping people turn their ideas into sites &amp; apps that work.
+          </Text>
         </div>
+        <div className={clsx("mx-auto text-center", "lg:mx-0")}>
+          <ContactDrawerButton label="Chat with me" />
+        </div>
+      </div>
+      <div
+        className={clsx(
+          "relative hidden",
+          "lg:block lg:col-span-3",
+          "xl:col-start-4",
+        )}
+      >
+        <Image src="./images/eddy.svg" alt="Eddy Sims" fill />
       </div>
     </Container>
   );
