6639b24616a4616f49f515e9d9b23fa3cbe11274
eddysims <eddysimsweb@gmail.com>
2024-12-04 16:11:18 -0700

    reverse proxy posthog

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -12,7 +12,8 @@ import type { PropsWithChildren } from "react";
 
 if (typeof window !== "undefined") {
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host: env.NEXT_PUBLIC_POSTHOG_HOST,
+    api_host: "/ingest",
+    ui_host: "https://us.posthog.com",
     person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
   });
 }
