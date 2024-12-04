243f0dd601ea3bf6e8e8249bac84ce1a8500b3e1
eddysims <eddysimsweb@gmail.com>
2024-12-04 15:55:24 -0700

    update the navigation

--- src/components/layout/Navigation/Navigation.tsx
+++ src/components/layout/Navigation/Navigation.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import NextLink from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 
@@ -9,105 +8,99 @@ import { cn } from "@/utils/cva";
 import { SocialLinks } from "@/components/social/SocialLinks";
 import { Icon } from "@/components/ui/Icon";
 
+import { useEventCapture } from "@/hooks/useEventCapture";
+
 import {
   NavigationItem,
   type NavigationItemProps,
 } from "./components/NavigationItem";
 
-const items: NavigationItemProps[] = [
-  {
-    label: "Eddy Sims",
-    caption: "Software Engineer",
-    href: "/",
-  },
-  {
-    label: "Blog",
-    caption: "Explore my writings",
-    href: "/blog",
-  },
-  {
-    label: "Contact Me",
-    caption: "Come say hi!",
-    href: "/contact",
-  },
-  {
-    label: "Resume",
-    caption: "Download it here",
-    href: "/pdfs/Eddy-Sims_Resume.pdf",
-    icon: "attachment-line",
-  },
-  {
-    label: "Storybook",
-    caption: "Over engineering required",
-    href: "https://storybook.eddysims.com/",
-    external: true,
-  },
-];
-
 export function Navigation() {
-  const pathname = usePathname();
-
   const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+  const capture = useEventCapture();
+
+  const menuItems: NavigationItemProps[] = [
+    {
+      label: "Eddy Sims",
+      href: "/",
+      caption: "Software Engineer",
+    },
+    {
+      label: "Blog",
+      href: "/blog",
+      caption: "Explore my writings",
+    },
+    {
+      label: "Contact Me",
+      href: "/contact",
+      caption: "Come say hi!",
+    },
+    {
+      label: "Resume",
+      href: "/pdfs/Eddy-Sims_Resume.pdf",
+      caption: "Download it here",
+      icon: "attachment-line",
+      onClick: () => {
+        capture("downloaded-resume", {
+          pathname,
+        });
+      },
+    },
+    {
+      label: "Storybook",
+      href: "https://storybook.eddysims.com/",
+      caption: "Over engineering required",
+    },
+  ];
 
   return (
-    <nav className={nav(isOpen)}>
-      <div className="mx-5">
-        <button
-          type="button"
-          aria-label="Toggle navigation"
-          className={toggle(isOpen)}
-          onClick={() => setIsOpen((prev) => !prev)}
-        >
-          <Icon icon={isOpen ? "close-line" : "menu-line"} size="lg" />
-        </button>
-        <NextLink
-          href="/"
-          aria-label="Go to the homepage"
-          className={readonlyIcon}
-        >
-          <Icon icon="menu-line" size="lg" />
-        </NextLink>
-      </div>
-      <ul className="grid gap-12">
-        {items.map((item) => (
-          <li key={item.href}>
-            <NavigationItem {...item} />
-          </li>
-        ))}
-      </ul>
+    <>
+      <div className={wrapper(isOpen)}>
+        <nav className="flex h-full w-screen flex-col justify-center px-1 md:w-72">
+          <span className="mx-5 -mt-20 mb-16 hidden text-slate-600 lg:inline-block">
+            <Icon icon="menu-line" size="lg" />
+          </span>
+          <ul className="grid gap-12">
+            {menuItems.map((item) => (
+              <NavigationItem
+                {...item}
+                key={item.href}
+                onClick={() => {
+                  setIsOpen((prev) => !prev);
+                  item.onClick?.();
+                }}
+              />
+            ))}
+          </ul>
 
-      {pathname !== "/" && (
-        <>
-          <hr className="mx-5 my-8 border-t border-slate-800 3xl:mx-0" />
-          <div className="mx-5">
-            <SocialLinks />
-          </div>
-        </>
-      )}
-    </nav>
+          {pathname !== "/" && (
+            <>
+              <hr />
+              <SocialLinks />
+            </>
+          )}
+        </nav>
+      </div>
+      <button
+        className="fixed left-5 top-8 z-50 lg:hidden"
+        type="button"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <Icon icon={isOpen ? "close-line" : "menu-line"} size="lg" />
+      </button>
+    </>
   );
 }
 
-const nav = (isOpen: boolean) =>
+const wrapper = (isOpen: boolean) =>
   cn(
-    "fixed top-0 z-50 flex h-dvh min-h-dvh w-full flex-col justify-center bg-slate-900/90 backdrop-blur transition-transform duration-300",
-    "md:w-80 md:border-r md:border-slate-800",
-    "lg:sticky lg:w-72 lg:border-0 lg:bg-transparent",
-    "3xl:border-r",
+    "fixed top-0 z-50 h-dvh min-h-dvh w-0 overflow-hidden bg-slate-900/90 backdrop-blur transition-all",
+    "border-r shadow-none duration-300",
+    "lg:sticky lg:top-0 lg:w-72 lg:border-0 lg:bg-transparent lg:duration-0",
+    "3xl:border-r 3xl:border-slate-800",
     {
-      "-translate-x-full lg:translate-x-0": !isOpen,
-      "translate-x-0": isOpen,
+      "w-screen border-slate-800 shadow-lg md:w-72": isOpen,
+      "border-transparent": !isOpen,
     },
   );
-
-const toggle = (isOpen: boolean) =>
-  cn("absolute top-12", "lg:hidden", {
-    "-right-12": !isOpen,
-    "left-6": isOpen,
-  });
-
-const readonlyIcon = cn(
-  "hidden rounded text-slate-700 outline-none",
-  "lg:absolute lg:-mt-20 lg:inline-block",
-  "focus:ring-2 focus:ring-slate-500 focus:ring-offset-2 focus:ring-offset-slate-900",
-);
--- src/components/layout/Navigation/components/NavigationItem.tsx
+++ src/components/layout/Navigation/components/NavigationItem.tsx
@@ -1,65 +1,68 @@
-import NextLink from "next/link";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 import { cn } from "@/utils/cva";
 
-import { type IconNames, Icon } from "@/components/ui/Icon";
+import { Icon, type IconNames } from "@/components/ui/Icon";
 
 export type NavigationItemProps = {
   label: string;
-  caption: string;
   href: string;
+  caption: string;
   icon?: IconNames;
-  external?: boolean;
   onClick?: () => void;
 };
 
 export function NavigationItem({
   label,
-  caption,
   href,
+  caption,
   icon,
-  external,
   onClick,
 }: NavigationItemProps) {
   const pathname = usePathname();
+  const isActive = () => {
+    if (href === "/") {
+      return pathname === href;
+    }
 
-  const isActive =
-    pathname === href || pathname.split("/")?.[1] === href.split("/")?.[1];
+    return pathname.startsWith(href);
+  };
+  const isExternal = href.startsWith("http");
   const isFile = href.endsWith(".pdf");
 
-  const linkProps = {
-    ...(external && { target: "_blank" }),
+  const conditionalProps = {
+    ...(isExternal && { target: "_blank" }),
+    ...(isExternal && { rel: "noreferrer" }),
     ...(isFile && { download: true }),
-    href,
-    onClick,
-    className: link(isActive),
   };
 
-  const Element = isFile ? "a" : NextLink;
+  const Element = isFile ? "a" : Link;
 
   return (
-    <Element {...linkProps}>
-      <span className="mb-0.5 flex items-center gap-2 font-semibold uppercase">
+    <Element
+      className={link(isActive())}
+      href={href}
+      onClick={onClick}
+      {...conditionalProps}
+    >
+      <span className="flex items-center gap-1.5 font-bold uppercase tracking-wider">
         {icon && <Icon icon={icon} size="sm" />}
         {label}
       </span>
-      <span className={style_caption}>{caption}</span>
+      <span className="text-sm font-semibold tracking-wide text-slate-400 transition-colors duration-300 group-hover:text-red-600 group-[.active]:text-red-600">
+        {caption}
+      </span>
     </Element>
   );
 }
 
 const link = (isActive: boolean) =>
   cn(
-    "group mx-1 block px-5 py-1 text-stone-100 outline-none transition duration-300 shadow-border-l",
-    "hover:text-red-600 hover:shadow-border-l-red-600",
-    "focus:text-red-600 focus:shadow-border-l-red-600",
+    "group grid gap-1 border-l-4 border-red-600 px-4 py-1.5 transition-colors duration-300",
+    "hover:border-red-600 hover:text-red-600",
     {
-      "active text-red-600 shadow-border-l-red-600": isActive,
+      "active border-red-600 text-red-600": isActive,
+      "border-transparent text-white": !isActive,
     },
   );
-
-const style_caption = cn(
-  "text-sm text-slate-400 transition duration-200",
-  "group-hover:text-red-600 group-focus:text-red-600 group-[.active]:text-red-600",
-);
