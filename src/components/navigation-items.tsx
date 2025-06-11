46aea97ca9f60d556572c694c4ccad0d6f2e8473
eddysims <eddysimsweb@gmail.com>
2025-06-11 17:10:08 -0600

    adds basic navigation items

---
+++ src/components/navigation-items.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+
+const MENU_ITEMS = [
+  {
+    label: "About me",
+    href: "/about",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+  {
+    label: "Hire me",
+    href: "/hire-me",
+    special: true,
+  },
+];
+
+export function NavigationItems() {
+  const pathname = usePathname();
+
+  return (
+    <nav className="w-full">
+      <ul className="flex w-full justify-center gap-6">
+        {MENU_ITEMS.map(({ label, href, special }) => (
+          <li key={href}>
+            <Link href={href} className={menuItem(special, pathname === href)}>
+              {label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+const menuItem = (special?: boolean, active?: boolean) =>
+  cn(
+    "hover:text-primary relative inline-flex py-3 tracking-wide lowercase",
+    "before:bg-primary after:bg-primary before:absolute before:-bottom-1.5 before:left-1/2 before:h-0.5 before:w-0 before:origin-center before:transition-[width] before:duration-300 before:ease-in-out after:absolute after:right-1/2 after:-bottom-1.5 after:h-0.5 after:w-0 after:origin-center after:transition-[width] after:duration-300 after:ease-in-out hover:before:w-1/3 hover:after:w-1/3",
+    {
+      "text-primary font-bold": special,
+    },
+    {
+      "text-primary before:w-1/3 after:w-1/3": active,
+    },
+  );
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,15 +1,22 @@
 import { ColorModeToggle } from "@/components/color-mode-toggle";
 import { Wrapper } from "@/components/ui//wrapper";
 import { Container } from "@/components/ui/container";
+import { cn } from "@/lib/utils";
+
+import { NavigationItems } from "./navigation-items";
 
 export function Navigation() {
   return (
     <Wrapper className="sticky top-0 mt-2 flex xl:mt-4">
-      <div className="hidden flex-1 p-2 xl:block" />
-      <Container className="flex items-center"> </Container>
-      <div className="hidden flex-1 p-2 xl:block">
+      <div className={spacer} />
+      <Container className="flex items-center">
+        <NavigationItems />
+      </Container>
+      <div className={spacer}>
         <ColorModeToggle />
       </div>
     </Wrapper>
   );
 }
+
+const spacer = cn("hidden flex-1 p-2 xl:block");
