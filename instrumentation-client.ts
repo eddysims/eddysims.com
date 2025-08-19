eab916117cc8aa060888cc92833870a82ab5bde4
eddysims <eddysimsweb@gmail.com>
2025-08-18 19:15:03 -0600

    add typed env vars

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,9 +1,12 @@
 import posthog from "posthog-js"
+import { env } from "@/lib/env/client";
 
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
-  ui_host: "https://us.posthog.com",
-  defaults: '2025-05-24',
-  capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
-  debug: process.env.NODE_ENV === "development",
-});
+if(env.NEXT_PUBLIC_POSTHOG_ENABLED) {
+  posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: "/ingest",
+    ui_host: "https://us.posthog.com",
+    defaults: '2025-05-24',
+    capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
+    debug: process.env.NODE_ENV === "development",
+  });
+}
