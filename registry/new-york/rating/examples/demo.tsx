3b030c52b876c7dc3364bb0b25daba616c1326e1
eddysims <eddysimsweb@gmail.com>
2025-08-21 11:17:19 -0600

    adds the demo component

--- registry/new-york/rating/examples/demo.tsx
+++ registry/new-york/rating/examples/demo.tsx
@@ -1,6 +1,6 @@
 import { Rating } from "@/components/ui/rating";
 
-export function Demo() {
+export default function Demo() {
   return (
     <div className="flex h-full items-center justify-center">
       <Rating value={3} readonly />
